74adb509fc574e5cb513aa698083c7942140d261
annadeu3 <annadeu3@gmail.com>
2016-01-20 18:38:37 -0500

    added tweetbank.js file and new updates to app.js

--- app.js
+++ app.js
@@ -2,36 +2,6 @@ var express = require( 'express');
 var app = express(); // creates an instance of an express application
 var swig = require('swig');
 
-app.get('/', function(request, response, next){
-	response.send("<h1>Welcome!</h1><p>Hello to you!</p>");
-	next();
-});
-
-app.get('/views/index.html', function(request, response, next){
-	response.send("<h1>Welcome!</h1><p>this is the index page!</p>");
-	next();
-});
-
-app.get('/', function(request, response, next){
-	console.log("something is happening here");
-	next();
-});
-
-app.get('/', function(request, response, next){
-	console.log("something is happening here");
-	next();
-});
-
-app.use('/', function(request, response, next){
-	console.log('Page is Loading');
-	var scode = response.statusCode;
-	console.log("GET / ", scode);
-	next();
-});
-app.use('/views/index.html', function(request, response, next){
-	console.log('index.html Page is Loading');
-	next();
-});
 
 var locals = {
     title: 'An Example',
@@ -46,17 +16,18 @@ swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) 
 });
 
 
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
 
 
-
-
-
-
-
-
-
-
-
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+app.get('/', function (req, res) {
+  res.render('index', {title: 'Hall of Fame', people: people});
+});
 
 
 
@@ -65,3 +36,35 @@ var port = 3000;
 app.listen(port, function() {
 	console.log('Server listening on port ', port);
 });
+
+
+// app.get('/', function(request, response, next){
+// 	response.send("<h1>Welcome!</h1><p>Hello to you!</p>");
+// 	next();
+// });
+
+// app.get('/views/index.html', function(request, response, next){
+// 	response.send("<h1>Welcome!</h1><p>this is the index page!</p>");
+// 	next();
+// });
+
+// app.get('/', function(request, response, next){
+// 	console.log("something is happening here");
+// 	next();
+// });
+
+// app.get('/', function(request, response, next){
+// 	console.log("something is happening here");
+// 	next();
+// });
+
+// app.use('/', function(request, response, next){
+// 	console.log('Page is Loading');
+// 	var scode = response.statusCode;
+// 	console.log("GET / ", scode);
+// 	next();
+// });
+// app.use('/views/index.html', function(request, response, next){
+// 	console.log('index.html Page is Loading');
+// 	next();
+// });
