466f80465393eeb08f4e34ffcbeb1ae3d785f823
annadeu3 <annadeu3@gmail.com>
2016-01-21 09:32:51 -0500

    trying to add id to the tweets

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,23 @@ router.get('/', function (req, res) {
   res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
 });
 
+router.get('/users/:id', function(req, res, next) {
+  var id = '@' +req.params.name;
+  var list = tweetBank.find( {id: id} );
+  var name = list[0].name;
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name, id: id, tweets: list } );
+});
+
+router.get('/tweet/:id', function(req,res,next){
+// 	// console.log(tweetBank.list());
+	var tweetid = request.params.id;
+// 	console.log(tweetid);
+// 	var list = tweetBank.find({'tweetid': tweetid});
+// 	if(list.length > 0){
+// 		response.render('index', {title: 'Twitter.js - Tweet #' + tweetid, tweets: list});
+// 	}else{
+// 		response.render('index', {title: 'Invalid tweet id', tweets:[]});
+// 	}
+ });
+
 module.exports = router;
