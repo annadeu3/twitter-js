9423461d546980850349c67a2ac1ad26772f6726
annadeu3 <annadeu3@gmail.com>
2016-01-21 09:29:49 -0500

    cleaned up required routes and listening to port number

--- app.js
+++ app.js
@@ -1,41 +1,23 @@
-var routes = require('./routes/');
+var swig = require('swig');
 var express = require('express');
 var app = express();
 var routes = require('./routes/');
-app.use('/', routes);
 
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
+swig.setDefaults({ cache: false });
 var tweetBank = require('./tweetBank');
-var swig = require('swig');
+
 
 
 app.use(express.static('public'));
-
-
-app.engine('html', swig.renderFile);
-
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+app.use('/', routes);
 app.set('view cache', false);
-swig.setDefaults({ cache: false });
 
 
 
+app.listen(3000);
 
-
-// swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
-//     console.log(output);
-// });
-
-
-
-
-
-
-
-
-var port = 3000;
-app.listen(port, function() {
-	console.log('Server listening on port ', port);
-});
 
