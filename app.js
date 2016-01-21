604d1f1dc1afadd40f8b6e4f1842c2102ebf9a73
annadeu3 <annadeu3@gmail.com>
2016-01-20 20:04:43 -0500

    first working twitter clone page works

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-var express = require( 'express');
-var app = express(); // creates an instance of an express application
+var routes = require('./routes/');
+var express = require('express');
+var app = express();
+var routes = require('./routes/');
+app.use('/', routes);
+
+
+var tweetBank = require('./tweetBank');
 var swig = require('swig');
 
 
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
-swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
-    console.log(output);
-});
+app.use(express.static('public'));
 
 
 app.engine('html', swig.renderFile);
@@ -24,10 +20,16 @@ app.set('view cache', false);
 swig.setDefaults({ cache: false });
 
 
-var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-app.get('/', function (req, res) {
-  res.render('index', {title: 'Hall of Fame', people: people});
-});
+
+
+
+// swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+//     console.log(output);
+// });
+
+
+
+
 
 
 
@@ -37,34 +39,3 @@ app.listen(port, function() {
 	console.log('Server listening on port ', port);
 });
 
-
-// app.get('/', function(request, response, next){
-// 	response.send("<h1>Welcome!</h1><p>Hello to you!</p>");
-// 	next();
-// });
-
-// app.get('/views/index.html', function(request, response, next){
-// 	response.send("<h1>Welcome!</h1><p>this is the index page!</p>");
-// 	next();
-// });
-
-// app.get('/', function(request, response, next){
-// 	console.log("something is happening here");
-// 	next();
-// });
-
-// app.get('/', function(request, response, next){
-// 	console.log("something is happening here");
-// 	next();
-// });
-
-// app.use('/', function(request, response, next){
-// 	console.log('Page is Loading');
-// 	var scode = response.statusCode;
-// 	console.log("GET / ", scode);
-// 	next();
-// });
-// app.use('/views/index.html', function(request, response, next){
-// 	console.log('index.html Page is Loading');
-// 	next();
-// });
